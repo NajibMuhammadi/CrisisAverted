7548068be37c6dadec6a1a22100cf114d6836879
Najib Muhammadi <najibmuhammadi003@gmail.com>
2024-04-16 10:34:14 +0200

    fixade api

--- Crisis-Averted/src/App.jsx
+++ Crisis-Averted/src/App.jsx
@@ -1,6 +1,15 @@
 import './app.css'
+import axios from 'axios'
+
+const getMovieApi = () => {
+  axios.get(`https://santosnr6.github.io/Data/movies.json`)
+    .then(response => {
+      console.log(response.data)
+    })
+}
 
 function App() {
+  getMovieApi()
   return (
     <h1>App</h1>
   )
