93c1c809dd55acd5c589311f893c6fa7ee30267e
Miklós Kisfaludy <kismiki2000@gmail.com>
2024-04-18 17:46:43 +0200

    fixed random-trailers

--- Crisis-Averted/src/components/trailer-component/TrailerList.jsx
+++ Crisis-Averted/src/components/trailer-component/TrailerList.jsx
@@ -1,16 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import PrevBtn from './PrevBtn';
 import NextBtn from './NextBtn';
 
 function TrailerList() {
   const [trailers, setTrailers] = useState([]);
-  const [currentTrailerIndex, setCurrentTrailerIndex] = useState(0);
+  const [randomMovie, setRandomMovie] = useState(null);
 
   useEffect(() => {
     axios.get(`https://santosnr6.github.io/Data/movies.json`)
       .then(response => {
         setTrailers(response.data);
+        
+        if (response.data.length > 0) {
+          const randomIndex = Math.floor(Math.random() * response.data.length);
+          setRandomMovie(response.data[randomIndex]);
+        }
       })
       .catch(error => {
         console.error('Error fetching movies:', error);
@@ -19,30 +24,36 @@ function TrailerList() {
   }, []);
 
   const handleToPrevTrailer = () => {
-    setCurrentTrailerIndex(prevIndex => prevIndex === 0 ? trailers.length - 1 : prevIndex - 1);
+    if (trailers.length > 0) {
+      const currentIndex = trailers.findIndex(movie => movie.imdbid === randomMovie.imdbid);
+      const prevIndex = (currentIndex - 1 + trailers.length) % trailers.length;
+      setRandomMovie(trailers[prevIndex]);
+    }
   };
 
   const handleNextTrailer = () => {
-    setCurrentTrailerIndex(prevIndex => (prevIndex + 1) % trailers.length);
+    if (trailers.length > 0) {
+      const currentIndex = trailers.findIndex(movie => movie.imdbid === randomMovie.imdbid);
+      const nextIndex = (currentIndex + 1) % trailers.length;
+      setRandomMovie(trailers[nextIndex]);
+    }
   };
 
   return (
     <div>
       <PrevBtn onClick={handleToPrevTrailer} />
       <ul className='trailer__list'>
-        {trailers.map((trailer, index) => (
-          <li key={index} className={index === currentTrailerIndex ? 'carousel__slide active' : 'carousel__slide'}>
-            {index === currentTrailerIndex && (
-              <iframe
-                src={trailer.trailer_link}
-                width="420"
-                height="315"
-                frameBorder="0"
-                title={trailer.title}
-              ></iframe>
-            )}
+        {randomMovie && (
+          <li className="carousel__slide" data-active>
+            <iframe
+              src={randomMovie.trailer_link}
+              width="420"
+              height="315"
+              frameBorder="0"
+              title={randomMovie.title}
+            ></iframe>
           </li>
-        ))}
+        )}
       </ul>
       <NextBtn onClick={handleNextTrailer} />
     </div>
